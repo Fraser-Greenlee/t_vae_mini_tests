0ffb2a1720512cd27fda1ae72e9ea8c8f057747b
Fraser-Greenlee <fraser.greenlee@mac.com>
2021-08-08 10:12:35 +0100

    .

--- mini_letter_order.sh
+++ mini_letter_order.sh
@@ -1,7 +1,9 @@
 python train_without_tokenizer.py \
-    --t5 "funnel-transformer/small" \
-    --use_t5_encoder \
+    --funnel_name "funnel-transformer/small" \
+    --t5 "t5-small" \
     --dataset_path 'tokenized_toy_problems.letter_ordering' \
+    --n_latent_tokens 1 \
+    --latent_size 2 \
     --max_train_samples 10000 \
     --max_eval_samples 100 \
-    
+
