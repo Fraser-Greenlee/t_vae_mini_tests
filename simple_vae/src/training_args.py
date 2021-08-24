763f201f03e37c30ee381e0c6e4d4df17e452626
Fraser-Greenlee <fraser.greenlee@mac.com>
2021-08-24 08:57:39 +0100

    .

--- simple_vae/src/training_args.py
+++ simple_vae/src/training_args.py
@@ -12,6 +12,8 @@ class VaeTrainingArguments(TrainingArguments):
     )
     reg_schedule_k: float = field(default=0.0025,   metadata={"help": "Multiplied by global_step in a sigmoid, more gradually increase regulariser loss weight."})
     reg_schedule_b: float = field(default=6.25,     metadata={"help": "Added to global step in sigmoid, further delays increase in regulariser loss weight."})
+    no_reg_loss: bool = field(default=False, metadata={"help": "Disable the VAE reg loss."})
+    recon_loss: bool = field(default=False, metadata={"help": "Use a reconstruction loss on input encodings."})
 
     def __post_init__(self):
         super().__post_init__()
